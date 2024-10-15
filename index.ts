ebb8d96a90959cadfdd4fb97fb898974f99a7c4c
Roman Sentsov <krug_ST@yahoo.com>
2024-10-14 23:29:52 -0700

    Adding more text to test Max characters is 4000 chars Adding get-chapter-names button

--- index.ts
+++ index.ts
@@ -1,18 +1,34 @@
 import { config } from "./src/config";
-import { Telegraf } from 'telegraf'
+import { Context, Telegraf } from 'telegraf'
 import { generateChapterNamesInline, getChapterContent } from './src/utils'
 const bot = new Telegraf(config.TELEGRAM_BOT_TOKEN);
 const bookChapterCount = 12; // Hardcoded for now
 const chapterNamesInline = generateChapterNamesInline(bookChapterCount, 5)
 
+
+const chapterNamesKeyboard = {
+    reply_markup: {
+        inline_keyboard: [
+            ...chapterNamesInline,
+        ]
+    }
+}
+
+const getChapterNamesKeyboard = {
+    reply_markup: {
+        inline_keyboard: [[
+            { text: `Оглавление`, callback_data: `get-chapter-names` }
+        ]]
+    }
+}
+
 bot.command("start", (ctx) => {
-    ctx.reply("Оглавление", {
-        reply_markup: {
-            inline_keyboard: [
-                ...chapterNamesInline,
-            ]
-        }
-    });
+    ctx.reply("Выберите главу:", chapterNamesKeyboard);
+
+});
+
+bot.action("get-chapter-names", (ctx) => {
+    ctx.reply("Оглавление", chapterNamesKeyboard);
 });
 
 for (let i = 1; i <= bookChapterCount; i++) {
@@ -20,7 +36,7 @@ for (let i = 1; i <= bookChapterCount; i++) {
     bot.action(chapterId, (ctx) => {
         console.log(ctx);
         const chapterContent = getChapterContent(chapterId);
-        ctx.reply(chapterContent);
+        ctx.reply(chapterContent, getChapterNamesKeyboard);
     });
 }
 
